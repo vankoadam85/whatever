98a206bc776c8710c7ba5cadde7ec8359aa3509e
Takács Róbert <89.t.robert@gmail.com>
2019-01-14 14:10:34 +0100

    Conflict Resolved

--- src/demo/Greet.java
+++ src/demo/Greet.java
@@ -3,8 +3,10 @@ package demo;
 public class Greet {
   public void greet() {
     System.out.println("Greetings!");
-    
+  }
+
   public void greet(String input) {
     System.out.println(input);
   }
 }
+
