ffe63f7eb72668b10a12dbc3032a0eeb3c827813
Vankó <vankoadam85@gmail.com>
2019-01-14 14:34:14 +0100

    c6

--- src/demo/Count.java
+++ src/demo/Count.java
@@ -2,8 +2,8 @@ package demo;
 
 public class Count {
 
-  public void count() {
-    System.out.println(10);
+  public void count(String input) {
+    System.out.println("Counting the countless " + input);
   }
 
 }
