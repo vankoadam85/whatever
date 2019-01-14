131f73ff0ec34126fdbc3617ecab37affe559df3
Takács Róbert <89.t.robert@gmail.com>
2019-01-14 12:56:15 +0100

    a1

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,8 +1,8 @@
 package demo;
 
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    Greet greet = new Greet();
+    greet.greet();
+  }
 }
