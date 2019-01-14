273432682b3da68337d79fc734fa278e01edeb16
Takács Róbert <89.t.robert@gmail.com>
2019-01-14 14:02:32 +0100

    a2

--- src/demo/Cheer.java
+++ src/demo/Cheer.java
@@ -1,3 +1,5 @@
+package demo;
+
 public class Cheer {
 
   public void cheer() {
--- src/demo/Greet.java
+++ src/demo/Greet.java
@@ -1,7 +1,7 @@
 package demo;
 
 public class Greet {
-  public void greet() {
-    System.out.println("Rob");
+  public void greet(String input) {
+    System.out.println(input);
   }
 }
